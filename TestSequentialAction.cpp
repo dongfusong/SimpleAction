6bacae13ec52999db86f5c9ff9c743d43bde129a
dongfusong <dongfusong@126.com>
2014-11-07 12:02:16 +0800

    start action project

---
+++ TestSequentialAction.cpp
@@ -0,0 +1,25 @@
+/*
+ * TestSequentialAction.cpp
+ *
+ *  Created on: 2014Äê11ÔÂ7ÈÕ
+ *      Author: Thoughtworks
+ */
+#include <gtest/gtest.h>
+class TestSequentialAction: public testing::Test {
+public:
+	void SetUp() {
+
+	}
+	void TearDown() {
+
+	}
+protected:
+};
+
+TEST_F(TestSequentialAction, )
+{
+
+}
+
+
+
